5fe8e5bdb7ae29f7f2544c25fe28325a77dc2fbe
iia7mt <a.ghannam18@student.aaup.edu>
2022-01-13 16:41:27 +0200

    the Main Class is clearer and seperated some loops in methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,37 +1,34 @@
 package com.company;
-
-
-import java.util.Arrays;
 import java.util.Random;
-
 public class Main {
 
-        public static void main(String[] args) {
-//          int []a={1,5,3,7,11,8,-8,-26,4,20,50,1,0,1,1,5,555555};
-//           int []a={5,5};
-//            int[] a =new int[1000000];
-//            Random rand=new Random();
-//            for (int i = 0; i <a.length ; i++) {
-//                a[i]=rand.nextInt(1000);
-//            }
-            int []a=new int[]{7,8,9,4,0,4,6,8,10};
-            for (int y:a) {
-                System.out.print(y+"\t");
-            }
-            System.out.println();
-            long startDate=System.nanoTime();
-
-            Sorting.medSort(a);
-
-            long endDate=System.nanoTime();
-
-            for (int y:a) {
-                System.out.print(y+"\t");
-
-            }
-            System.out.println();
-            System.out.println("the time is : "+(endDate-startDate)+"ns");
-        }
-
-
+    public static void main(String[] args) {
+        int[] aa = {1, 5, 3, 7, 11, 8, -8, -26, 4, 20, 50, 1, 0, 1, 1, 5, 555555};
+        int[] aaaa = {5, 5};
+        int[] aaa = new int[1000000];
+        addRandomNumbers(aa);
+        int[] a = new int[]{7, 8, 9, 4, 0, 4, 6, 8, 10};
+        printArray(a);
+        System.out.println();
+        long startDate = System.nanoTime();
+        Sorting.medSort(a);
+        long endDate = System.nanoTime();
+        printArray(a);
+        System.out.println();
+        System.out.println("the time is : " + (endDate - startDate) + "ns");
     }
+    private static void addRandomNumbers(int[] aa) {
+        Random rand = new Random();
+        for (int i = 0; i < aa.length; i++) {
+            aa[i] = rand.nextInt(1000);
+        }
+    }
+    private static void printArray(int[] a) {
+        for (int y : a) {
+            System.out.print(y + "\t");
+        }
+        System.out.println();
+    }
+
+
+}
