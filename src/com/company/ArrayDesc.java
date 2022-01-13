22e47e9b178556ceb6a614bb0ab50c6c9141f99e
iia7mt <a.ghannam18@student.aaup.edu>
2022-01-13 23:18:10 +0200

    trying to make code more clear and understandable

--- src/com/company/ArrayDesc.java
+++ src/com/company/ArrayDesc.java
@@ -1,33 +1,34 @@
 package com.company;
 
 public class ArrayDesc {
-   private int min;
+
+    //the attributes
+    private int min;
     private int max;
 
+    // constructor
     public ArrayDesc(int min, int max) {
         this.min = min;
         this.max = max;
     }
-
+    //getters and setters
     public int getMin() {
         return min;
     }
-
     public void setMin(int min) {
         this.min = min;
     }
-
     public int getMax() {
         return max;
     }
-
     public void setMax(int max) {
         this.max = max;
     }
+    // other methods
     public Boolean minEqualsMax(){
         return min==max;
     }
-   public int getMedian(){
+    public int getMedian(){
         return ((min+max)/2);
    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,17 +4,20 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        int[] aa = {1, 5, 3, 7, 11, 8, -8, -26, 4, 20, 50, 1, 0, 1, 1, 5, 555555};
-        int[] aaaa = {5, 5};
-        int[] aaa = new int[1000000];
-        addRandomNumbers(aaa);
-        int[] a = new int[]{7, 8, 9, 4, 0, 4, 6, 8, 10};
-        printArray(aa);
+//        int[] a = {1, 5, 3, 7, 11, 8, -8, -26, 4, 20, 50, 1, 0, 1, 1, 5, 555555};
+//        int[] a = {5, 5};
+        int[] a = new int[1000000];
+        addRandomNumbers(a);
+//        int[] a = new int[]{7, 8, 9, 4, 0, 4, 6, 8, 10};
+        printArray(a);
         System.out.println();
+
         long startDate = System.nanoTime();
-        Sorting.medSort(aa);
+
+        Sorting.medSort(a);
+
         long endDate = System.nanoTime();
-        printArray(aa);
+        printArray(a);
         System.out.println();
         System.out.println("the time is : " + (endDate - startDate) + "ns");
     }
